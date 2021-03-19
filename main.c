917ec1a4d9bab829c30df6de82e9cdba6ddd8ec5
Zach Marcus <55953922+zachmarcus7@users.noreply.github.com>
2021-03-18 18:32:00 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -25,7 +25,28 @@
 
 
 /*********************************************************************
-* Global variables, for use with foreground-only mode
+* Container for shell variables.
+*********************************************************************/
+struct shellVars {
+	bool run;
+	bool background;
+	char userInput[2048];
+	char* userArguments[512];
+	char* savePtr;
+	int index;
+	pid_t childPid;
+	int childStatus;
+	int inputFileDesc;
+	int outputFileDesc;
+	char* inputFileName;
+	char* outputFileName;
+	int result;
+	struct sigaction interruptSignal;
+};
+
+
+/*********************************************************************
+* Global variables, for use with foreground-only mode.
 *********************************************************************/
 bool foregroundMode = 0;
 bool signalReceived = 0;
@@ -110,6 +131,311 @@ void expandVariable(char* array) {
 
 
 /*********************************************************************
+* Function:    getInput
+* Description: This function gets the user input, parses it into tokens,
+*              while also checking for certain characters. Returns 1 if
+*              shell loop is meant to re-loop, otherwise it returns 2.
+* Parameters:  struct shellVars*
+* Returns:     int
+*********************************************************************/
+int getInput(struct shellVars *shell) {
+
+	//get the user's input
+	printf(": ");
+	fflush(stdout);
+	fgets(shell->userInput, 2048, stdin);
+
+	//separate out each piece of user input
+	char* argument = strtok_r(shell->userInput, " \n", &shell->savePtr);
+	shell->index = 0;
+	shell->background = 0;
+
+	//check if nothing was entered
+	if (argument == NULL)
+		return 1;
+
+	//store each argument into userArguments array
+	while (argument != NULL) {
+
+		//check if input symbol has been encountered
+		if (!strcmp(argument, "<")) {
+			argument = strtok_r(NULL, " \n", &shell->savePtr);
+			shell->inputFileName = strdup(argument);
+			argument = strtok_r(NULL, " \n", &shell->savePtr);
+			continue;
+		}
+
+		//check if output symbol has been encountered
+		if (!strcmp(argument, ">")) {
+			argument = strtok_r(NULL, " \n", &shell->savePtr);
+			shell->outputFileName = strdup(argument);
+			argument = strtok_r(NULL, " \n", &shell->savePtr);
+			continue;
+		}
+
+		shell->userArguments[shell->index] = strdup(argument);
+		argument = strtok_r(NULL, " \n", &shell->savePtr);
+
+		//check if final argument is a &, for background command
+		if (argument == NULL) {
+			if (!strcmp(shell->userArguments[shell->index], "&")) {
+				shell->background = 1;
+				break;
+			}
+		}
+		shell->index++;
+	}
+
+	//set the last element to NULL, for use with exec functions
+	shell->userArguments[shell->index] = NULL;
+
+	//expand the $$ variable
+	for (int i = 0; i < shell->index; i++)
+		expandVariable(shell->userArguments[i]);
+
+	return 2;
+}
+
+
+/*********************************************************************
+* Function:    testInput
+* Description: This function tests the user input. Returns 1 if
+*              shell loop is meant to re-loop, otherwise it returns 2.
+* Parameters:  struct shellVars*
+* Returns:     int
+*********************************************************************/
+int testInput(struct shellVars* shell) {
+
+	//if user entered exit
+	if (!strcmp(shell->userArguments[0], "exit")) {
+		shell->run = 0;
+		exit(0);
+	}
+	//if user entered cd 
+	else if (!strcmp(shell->userArguments[0], "cd")) {
+		//check if there were arguments passed with cd
+		if (shell->userArguments[1] == NULL)
+			chdir(getenv("HOME"));
+		else {
+			if (chdir(shell->userArguments[1]) == -1) {
+				printf("No such directory\n");
+				fflush(stdout);
+			}
+		}
+	}
+	//if user entered status
+	else if (!strcmp(shell->userArguments[0], "status")) {
+		//if child process terminated normally
+		if (WIFEXITED(shell->childStatus)) {
+			printf("exit value %d\n", WEXITSTATUS(shell->childStatus));
+			fflush(stdout);
+		}
+		else {
+			printf("terminated by signal %d\n", WTERMSIG(shell->childStatus));
+			fflush(stdout);
+		}
+	}
+	//if user entered a comment
+	else if (*shell->userArguments[0] == '#') {
+		return 1;
+	}
+	//if foreground-only signal was received
+	else if (signalReceived) {
+		signalReceived = 0;
+		return 1;
+	}
+	//if user entered a different command
+	else {
+		//create a new process
+		shell->childPid = fork();
+
+		//check what kind of process is running
+		switch (shell->childPid) {
+		//error in creating process
+		case -1:
+			shell->childStatus = 1;
+			break;
+		//child process 
+		case 0:
+			//if the process is a background command
+			if (shell->background) {
+				//redirect standard in to /dev/null
+				shell->inputFileDesc = open("/dev/null", O_RDONLY);
+				if (shell->inputFileDesc == -1) {
+					printf("cannot open\n");
+					fflush(stdout);
+					exit(1);
+				}
+
+				//change standard in (0) to inputFileName
+				shell->result = dup2(shell->inputFileDesc, 0);
+				if (shell->result == -1) {
+					exit(1);
+				}
+
+				//redirect standard out to /dev/null
+				shell->outputFileDesc = open("/dev/null", O_RDWR | O_CREAT | O_TRUNC, 0777);
+				if (shell->outputFileDesc == -1) {
+					printf("cannot open\n");
+					fflush(stdout);
+					exit(1);
+				}
+
+				//change standard out (1) to outputFileName
+				shell->result = dup2(shell->outputFileDesc, 1);
+				if (shell->result == -1) {
+					exit(1);
+				}
+			}
+
+			//input redirection
+			if (shell->inputFileName != NULL) {
+
+				//open the file for reading only
+				shell->inputFileDesc = open(shell->inputFileName, O_RDONLY);
+				if (shell->inputFileDesc == -1) {
+					printf("cannot open %s for input\n", shell->inputFileName);
+					fflush(stdout);
+					exit(1);
+				}
+
+				//change standard in (0) to inputFileName
+				shell->result = dup2(shell->inputFileDesc, 0);
+				if (shell->result == -1) {
+					exit(1);
+				}
+				close(shell->inputFileDesc);
+			}
+
+			//output redirection
+			if (shell->outputFileName != NULL) {
+
+				//open the file for reading/writing, if it's not there create it, or truncate onto it
+				shell->outputFileDesc = open(shell->outputFileName, O_RDWR | O_CREAT | O_TRUNC, 0777);
+				if (shell->outputFileDesc == -1) {
+					printf("cannot open %s for output\n", shell->outputFileName);
+					fflush(stdout);
+					exit(1);
+				}
+
+				//change standard out (1) to outputFileName
+				shell->result = dup2(shell->outputFileDesc, 1);
+				if (shell->result == -1) {
+					exit(1);
+				}
+				close(shell->outputFileDesc);
+			}
+
+			//if the process is a foreground process
+			if (shell->background == 0) {
+
+				//foreground child process terminates with SIGINT
+				shell->interruptSignal.sa_handler = SIG_DFL;
+				shell->interruptSignal.sa_flags = 0;
+				sigaction(SIGINT, &shell->interruptSignal, NULL);
+			}
+			//if foreground mode is enabled
+			else if (foregroundMode) {
+
+				//if in foreground-only mode, background process terminates with SIGINT
+				shell->interruptSignal.sa_handler = SIG_DFL;
+				shell->interruptSignal.sa_flags = 0;
+				sigaction(SIGINT, &shell->interruptSignal, NULL);
+			}
+
+			//execute the command
+			execvp(shell->userArguments[0], shell->userArguments);
+			printf("%s: no such file or directory\n", shell->userArguments[0]);
+			exit(2);
+			break;
+			//parent process
+		default:
+			//if foregroundMode is active, parent must wait till all processes complete
+			if (foregroundMode) {
+				shell->childPid = waitpid(shell->childPid, &shell->childStatus, 0);
+
+				//check if a SIGINT was sent
+				if (WIFSIGNALED(shell->childStatus)) {
+					printf("terminated by signal %d\n", WTERMSIG(shell->childStatus));
+					fflush(stdout);
+				}
+			}
+			//if background process, command line access immediately returned
+			else if (shell->background) {
+				printf("background pid is %d\n", shell->childPid);
+				fflush(stdout);
+			}
+			//foreground process, parent must wait till it completes
+			else {
+
+				shell->childPid = waitpid(shell->childPid, &shell->childStatus, 0);
+
+				//check if a SIGINT was sent
+				if (WIFSIGNALED(shell->childStatus)) {
+					printf("terminated by signal %d\n", WTERMSIG(shell->childStatus));
+					fflush(stdout);
+				}
+			}
+			break;
+		}
+	}
+	return 2;
+}
+
+
+/*********************************************************************
+* Function:    checkBackground
+* Description: This function checks if any background processes have 
+*              terminated.
+* Parameters:  struct shellVars*
+* Returns:     void
+*********************************************************************/
+void checkBackground(struct shellVars* shell) {
+
+	//have waitpid wait for any child process, WNOHANG returns immediately if no child process has terminated
+	shell->childPid = waitpid(-1, &shell->childStatus, WNOHANG);
+
+	//enter a loop, clearing out remaining  child processes waiting to be terminated
+	while (shell->childPid > 0) {
+
+		if (WIFEXITED(shell->childStatus)) {
+			printf("background pid %d is done: exit value %d \n", shell->childPid, WEXITSTATUS(shell->childStatus));
+			fflush(stdout);
+		}
+		else {
+			printf("background pid %d is done: terminated by signal %d\n", shell->childPid, WTERMSIG(shell->childStatus));
+			fflush(stdout);
+		}
+
+		//get the process id for the next background process waiting to be terminated
+		shell->childPid = waitpid(-1, &shell->childStatus, WNOHANG);
+	}
+
+}
+
+
+/*********************************************************************
+* Function:    resetPtrs
+* Description: This function takes a pointer to a shell struct and resets
+*              the pointers inside of it to NULL, so that they're ready
+*              for use with the next input.
+* Parameters:  struct shellVars*
+* Returns:     void
+*********************************************************************/
+void resetPtrs(struct shellVars* shell) {
+
+	//reset character pointers for input/output files
+	shell->inputFileName = NULL;
+	shell->outputFileName = NULL;
+
+	//reset userArgument pointers
+	for (int i = 0; i < shell->index; i++) 
+		free(shell->userArguments[i]);
+
+}
+
+
+/*********************************************************************
 * Function:    main
 * Description: This is the main function for the shell. It's separated
 *              into three main parts: getting the user input, checking the user
@@ -118,20 +444,6 @@ void expandVariable(char* array) {
 * Returns:     EXIT_SUCCESS
 *********************************************************************/
 int main(int argc, char* argv[]) {
-	//initialize variables for running the shell
-	bool shell = 1;
-	bool background;
-	char userInput[2048];
-	char* userArguments[512];
-	char* savePtr;
-	int index;
-	pid_t childPid;
-	int childStatus = 0;
-	int inputFileDesc = 0;
-	int outputFileDesc = 0;
-	char* inputFileName = NULL;
-	char* outputFileName = NULL;
-	int result;
 
 	//create signal handler for SIGINT
 	struct sigaction interruptSignal;
@@ -146,272 +458,23 @@ int main(int argc, char* argv[]) {
 	stopSignal.sa_flags = 0;
 	sigaction(SIGTSTP, &stopSignal, NULL);
 
+	//initialize variables for running the shell
+	struct shellVars shell = { .run = 1, .inputFileName = NULL, .outputFileName = NULL, .interruptSignal = interruptSignal };
+	struct shellVars* shellPtr = &shell;
 
-	//run the shell
-	while (shell) {
 
+	while (shell.run) {
 
-		/* get user input, store it inside userArguments array */
-
-		printf(": ");
-		fflush(stdout);
-		fgets(userInput, 2048, stdin);
-		
-		//separate out each piece of user input
-		char* argument = strtok_r(userInput, " \n", &savePtr);
-		index = 0;
-		background = 0;
-
-		//check if nothing was entered
-		if (argument == NULL)
+		//get user input, then test it
+		if (getInput(shellPtr) == 1)
 			continue;
 
-		//store each argument into userArguments array
-		while (argument != NULL) {
-
-			//check if input symbol has been encountered
-			if (!strcmp(argument, "<")) {
-				argument = strtok_r(NULL, " \n", &savePtr);
-				inputFileName = strdup(argument);
-				argument = strtok_r(NULL, " \n", &savePtr);
-				continue;
-			}
-
-			//check if output symbol has been encountered
-			if (!strcmp(argument, ">")) {
-				argument = strtok_r(NULL, " \n", &savePtr);
-				outputFileName = strdup(argument);
-				argument = strtok_r(NULL, " \n", &savePtr);
-				continue;
-			}
-
-			userArguments[index] = strdup(argument);
-			argument = strtok_r(NULL, " \n", &savePtr);
-
-			//check if final argument is a &, for background command
-			if (argument == NULL) {
-				if (!strcmp(userArguments[index], "&")) {
-					background = 1;
-					break;
-				}
-			}
-			index++;
-		}
-
-		//set the last element to NULL, for use with exec functions
-		userArguments[index] = NULL;
-
-		//expand the $$ variable
-		for (int i = 0; i < index; i++) 
-			expandVariable(userArguments[i]);
-		
-
-		/* test user input */
-
-		//if user entered exit
-		if (!strcmp(userArguments[0], "exit")) {
-			shell = 0;
-			exit(0);
-		}
-		//if user entered cd 
-		else if (!strcmp(userArguments[0], "cd")) {
-			//check if there were arguments passed with cd
-			if (userArguments[1] == NULL)
-				chdir(getenv("HOME"));
-			else {
-				if (chdir(userArguments[1]) == -1) {
-					printf("No such directory\n");
-					fflush(stdout);
-				}
-			}
-		}
-		//if user entered status
-		else if (!strcmp(userArguments[0], "status")) {
-			//if child process terminated normally
-			if (WIFEXITED(childStatus)) {
-				printf("exit value %d\n", WEXITSTATUS(childStatus));
-				fflush(stdout);
-			} 
-			else {
-				printf("terminated by signal %d\n", WTERMSIG(childStatus));
-				fflush(stdout);
-			}
-		}
-		//if user entered a comment
-		else if (*userArguments[0] == '#') {
+		if (testInput(shellPtr) == 1)
 			continue;
-		}
-		//if foreground-only signal was received
-		else if (signalReceived) {
-			signalReceived = 0;
-			continue;
-		}
-		//if user entered a different command
-		else {
-			//create a new process
-			childPid = fork();
 
-			//check what kind of process is running
-			switch (childPid) {
-				//error in creating process
-				case -1:
-					childStatus = 1;
-					break;
-				//child process 
-				case 0: 
-					//if the process is a background command
-					if (background) {
-						//redirect standard in to /dev/null
-						inputFileDesc = open("/dev/null", O_RDONLY);
-						if (inputFileDesc == -1) {
-							printf("cannot open\n");
-							fflush(stdout);
-							exit(1);
-						}
-
-						//change standard in (0) to inputFileName
-						result = dup2(inputFileDesc, 0);
-						if (result == -1) {
-							exit(1);
-						}
-
-						//redirect standard out to /dev/null
-						outputFileDesc = open("/dev/null", O_RDWR | O_CREAT | O_TRUNC, 0777);
-						if (outputFileDesc == -1) {
-							printf("cannot open\n");
-							fflush(stdout);
-							exit(1);
-						}
-
-						//change standard out (1) to outputFileName
-						result = dup2(outputFileDesc, 1);
-						if (result == -1) {
-							exit(1);
-						}
-					}
-
-					//input redirection
-					if (inputFileName != NULL) {
-
-						//open the file for reading only
-						inputFileDesc = open(inputFileName, O_RDONLY);
-						if (inputFileDesc == -1) {
-							printf("cannot open %s for input\n", inputFileName);
-							fflush(stdout);
-							exit(1);
-						}
-
-						//change standard in (0) to inputFileName
-						result = dup2(inputFileDesc, 0);
-						if (result == -1) {
-							exit(1);
-						}
-						close(inputFileDesc);
-					}
-
-					//output redirection
-					if (outputFileName != NULL) {
-
-						//open the file for reading/writing, if it's not there create it, or truncate onto it
-						outputFileDesc = open(outputFileName, O_RDWR | O_CREAT | O_TRUNC, 0777);
-						if (outputFileDesc == -1) {
-							printf("cannot open %s for output\n", outputFileName);
-							fflush(stdout);
-							exit(1);
-						}
-
-						//change standard out (1) to outputFileName
-						result = dup2(outputFileDesc, 1);
-						if (result == -1) {
-							exit(1);
-						}
-						close(outputFileDesc);
-					}
-
-					//if the process is a foreground process
-					if (background == 0) {
-
-						//foreground child process terminates with SIGINT
-						interruptSignal.sa_handler = SIG_DFL;
-						interruptSignal.sa_flags = 0;
-						sigaction(SIGINT, &interruptSignal, NULL);
-					} 
-					//if foreground mode is enabled
-					else if (foregroundMode) {
-
-						//if in foreground-only mode, background process terminates with SIGINT
-						interruptSignal.sa_handler = SIG_DFL;
-						interruptSignal.sa_flags = 0;
-						sigaction(SIGINT, &interruptSignal, NULL);
-					}
-
-					//execute the command
-					execvp(userArguments[0], userArguments);
-					printf("%s: no such file or directory\n", userArguments[0]);
-					exit(2);
-					break;
-				//parent process
-				default:
-					//if foregroundMode is active, parent must wait till all processes complete
-					if (foregroundMode) {
-						childPid = waitpid(childPid, &childStatus, 0);
-
-						//check if a SIGINT was sent
-						if (WIFSIGNALED(childStatus)) {
-							printf("terminated by signal %d\n", WTERMSIG(childStatus));
-							fflush(stdout);
-						}
-					}
-					//if background process, command line access immediately returned
-					else if (background) {
-						printf("background pid is %d\n", childPid);
-						fflush(stdout);
-					}
-					//foreground process, parent must wait till it completes
-					else {
-
-						childPid = waitpid(childPid, &childStatus, 0);
-						
-						//check if a SIGINT was sent
-						if (WIFSIGNALED(childStatus)) {
-							printf("terminated by signal %d\n", WTERMSIG(childStatus));
-							fflush(stdout);
-						}
-					}
-					break;
-			}	
-		}
-
-
-		/* check if any background processes have terminated */
-
-		//have waitpid wait for any child process, WNOHANG returns immediately if no child process has terminated
-		childPid = waitpid(-1, &childStatus, WNOHANG);
-
-		//enter a loop, clearing out remaining  child processes waiting to be terminated
-		while (childPid > 0) { 
-
-			if (WIFEXITED(childStatus)) {
-				printf("background pid %d is done: exit value %d \n", childPid, WEXITSTATUS(childStatus));
-				fflush(stdout);
-			}
-			else {
-				printf("background pid %d is done: terminated by signal %d\n", childPid, WTERMSIG(childStatus));
-				fflush(stdout);
-			}
-
-			//get the process id for the next background process waiting to be terminated
-			childPid = waitpid(-1, &childStatus, WNOHANG);
-		}
-
-		//reset character pointers for input/output files
-		inputFileName = NULL;
-		outputFileName = NULL;
-
-		//reset userArgument pointers
-		for (int i = 0; i < index; i++) {
-			free(userArguments[i]);
-		}
+		checkBackground(shellPtr);
+		resetPtrs(shellPtr);
 	}
+
 	return EXIT_SUCCESS;
 }
